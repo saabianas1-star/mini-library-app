73882b775d324f814d99b54120b764827a5c9ac8
saabianas1-star <saabianas1@gmail.com>
2026-09-22 22:33:52 +0300

    Update add_book_screen.dart

--- lib/screens/add_book_screen.dart
+++ lib/screens/add_book_screen.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter/material.dart';
 import '../models/book.dart';
+import 'package:file_picker/file_picker.dart';
 
 class AddBookScreen extends StatefulWidget {
   const AddBookScreen({super.key});
