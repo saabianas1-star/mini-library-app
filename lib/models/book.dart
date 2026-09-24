b670300c4c7e5c1a6cc3cd8379a76b0ff04e5625
saabianas1-star <saabianas1@gmail.com>
2026-09-24 04:27:38 +0300

    Update book.dart

--- lib/models/book.dart
+++ lib/models/book.dart
@@ -14,4 +14,25 @@ class Book {
     this.coverPath,
     required this.pdfPath,
   });
+  Map<String, dynamic> toMap() {
+  return {
+    'title': title,
+    'author': author,
+    'description': description,
+    'price': price,
+    'coverPath': coverPath,
+    'pdfPath': pdfPath,
+  };
+}
+
+factory Book.fromMap(Map<String, dynamic> map) {
+  return Book(
+    title: map['title'],
+    author: map['author'],
+    description: map['description'],
+    price: map['price'],
+    coverPath: map['coverPath'],
+    pdfPath: map['pdfPath'],
+  );
+}
 }
